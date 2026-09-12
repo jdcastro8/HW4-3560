956c44219b39337c65569fd3699a06e3a8168e41
Julian <juliancastro@cpp.edu>
2026-09-11 18:00:20 -0700

    Add Employee class with basic attributes and methods

---
+++ Problem1/Employee.java
@@ -0,0 +1,40 @@
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String socialSecurityNumber;
+
+    public Employee(String firstName, String lastName, String socialSecurityNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void print() {
+        System.out.println("Name: " + firstName + " " + lastName);
+        System.out.println("SSN: " + socialSecurityNumber);
+    }
+}
