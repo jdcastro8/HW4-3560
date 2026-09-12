7e2091fa1dbe85569b8cd4dcd7b42c3f1a3f5035
Julian <juliancastro@cpp.edu>
2026-09-11 18:05:45 -0700

    Add CruiseShip class extending Ship

---
+++ Problem3/CruiseShip.java
@@ -0,0 +1,22 @@
+public class CruiseShip extends Ship {
+    private int maxPassengers;
+
+    public CruiseShip(String name, String yearBuilt, int maxPassengers) {
+        super(name, yearBuilt);
+        this.maxPassengers = maxPassengers;
+    }
+
+    public void setMaxPassengers(int maxPassengers) {
+        this.maxPassengers = maxPassengers;
+    }
+
+    public int getMaxPassengers() {
+        return maxPassengers;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Cruise Ship Name: " + getName());
+        System.out.println("Maximum Passengers: " + maxPassengers);
+    }
+}
